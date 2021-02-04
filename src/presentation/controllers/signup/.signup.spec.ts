2ba76864f40d353983efbb7a32fe7a5f75a666cc
Joao Pedro <joaopedro7423@hotmail.com>
2021-02-04 16:43:30 -0300

    refactor: move all signup protocols to a single file

--- src/presentation/controllers/signup/.signup.spec.ts
+++ src/presentation/controllers/signup/.signup.spec.ts
@@ -1,9 +1,6 @@
 import { SignUpController } from './signup'
-import {EmailValidator} from '../protocols'
-import { MissingParamError,InvalidParamError, ServerError } from '../errors'
-import {AccountModel} from '../../domain/models/account'
-import {AddAccount,AddAccountModel} from '../../domain/usecases/add-account'
-
+import {EmailValidator, AccountModel,AddAccount,AddAccountModel } from './signup-protocols'
+import { MissingParamError,InvalidParamError, ServerError } from '../../errors'
 
     //cria uma variavel  recebe nenhum parametro : retorna o azul ae kkkk
 const makeEmailValidator = (): EmailValidator =>{
