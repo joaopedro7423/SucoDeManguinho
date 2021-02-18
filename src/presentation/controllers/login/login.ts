576edc0617c9e5a515b6be20cc809a66c75b218f
Joao Pedro <joaopedro7423@hotmail.com>
2021-02-18 16:15:07 -0300

    feat: test realized

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, ok, serverError } from '../../helpers/http-helper'
+import { badRequest, ok } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../signup/signup-protocols'
 
@@ -11,23 +11,16 @@ export class LoginControler implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { email, password } = httpRequest.body
-    try {
-      if (!email) {
-        return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
-      }
-      if (!password) {
-        return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
-      }
-      const isValid = this.emailvalidator.isValid(email)
-      if (!isValid) {
-        return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
-      }
-      return ok(isValid)
-    } catch (error) {
-      // outra forma de mostrar um erro sem usar o npm test, mais aconselhavel utilizar nos debug:
-      // console.error(error)
-
-      return serverError(error)
+    if (!email) {
+      return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
     }
+    if (!password) {
+      return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
+    }
+    const isValid = this.emailvalidator.isValid(email)
+    if (!isValid) {
+      return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+    }
+    return ok(isValid)
   }
 }
--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,5 +1,5 @@
 export class ServerError extends Error {
-  constructor (stack: string) {
+  constructor (stack: any) {
     super('A Porra do servidor caiu seu merda, a caralha do manguinho que pediu para colocar a merda de uma mensagem amigavel seu lixo.')
     this.name = 'ServerError'
     this.stack = stack
