03909c7b4053f881ae80b0cbe87b4bea68de5217
Joao Pedro <joaopedro7423@hotmail.com>
2021-02-24 13:28:25 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,9 +1,9 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
-import env from '../../main/config/env'
+import env from '../config/env'
 
-describe('SignUp Routes',() => {
+describe('Login Routes',() => {
   beforeAll(async () => {
     await MongoHelper.connect(env.mongoUrl)
   })
@@ -17,15 +17,17 @@ describe('SignUp Routes',() => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'JooJ',
-        email: 'jooj@gmail.com',
-        password: '123',
-        passwordConfirmation: '123'
-      })
-      .expect(200)
+  describe('Post / signup',() => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'JooJ',
+          email: 'jooj@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
   })
 })
