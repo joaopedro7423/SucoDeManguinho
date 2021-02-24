8bd854b9c23738c13fec2a2b8161cb9b88a9c0ce
Joao Pedro <joaopedro7423@hotmail.com>
2021-02-24 15:13:54 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,5 +51,15 @@ describe('SignUp Routes',() => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'jooj@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
