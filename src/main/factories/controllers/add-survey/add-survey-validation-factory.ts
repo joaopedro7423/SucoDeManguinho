e9c809c110f56c9be2149cdcfc77059a75300626
Joao Pedro <joaopedro7423@hotmail.com>
2021-03-05 13:20:21 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.ts
@@ -0,0 +1,11 @@
+import { ValidationComposite, RequiredFieldValidation } from '../../../../validation/validators'
+import { Validation } from '../../../../presentation/protocols/validation'
+
+export const makeAddSurveyValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  // aqui se adiciona o campo para a validação
+  for (const field of ['question', 'answers']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
