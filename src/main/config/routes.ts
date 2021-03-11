6416ef8254443e32911b0ceaaf419bfeb117402b
Joao Pedro <joaopedro7423@hotmail.com>
2021-03-11 14:39:42 -0300

    fix: make controllers returns the error message instead of the error

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -7,7 +7,7 @@ export default (app: Express): void => {
   const path = require('path')
   const test = path.join(`${__dirname}`,'/../routes')
   readdirSync(test).map(async file => {
-    if (!file.includes('.test.')) {
+    if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
       console.log(file)
     }
