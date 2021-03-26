001ca9361d889b8cc30ffbccc8e38c09314b9ce5
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-03-26 16:48:31 -0400

    refactor: remake tests from data layer in usecases

--- src/data/usecases/account/authentication/db-authentication.ts
+++ src/data/usecases/account/authentication/db-authentication.ts
@@ -8,10 +8,10 @@ export class DbAuthentication implements Authentication {
     private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) { }
 
-  async auth (authentication: AuthenticationParams): Promise<string> {
-    const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
+  async auth (authenticationParams: AuthenticationParams): Promise<string> {
+    const account = await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.email)
     if (account) {
-      const isValid = await this.hashComparer.compare(authentication.password,account.password) // aqui que chama as funções do arquivo de teste, olha só que legal. pqp
+      const isValid = await this.hashComparer.compare(authenticationParams.password,account.password) // aqui que chama as funções do arquivo de teste, olha só que legal. pqp
       if (isValid) {
         const accessToken = await this.encrypter.encrypt(account.id)
         await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
