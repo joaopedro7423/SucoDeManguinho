b634c58f42c1144dd4cc0caa67162c2a14446436
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-03-29 13:51:54 -0400

    feat: add route to serve static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
 import express from 'express'
+import setupStaticFiles from './static-files'
 
 const app = express()
+setupStaticFiles(app)
 setupMiddlewares(app)
 setupRoutes(app)
 export default app
