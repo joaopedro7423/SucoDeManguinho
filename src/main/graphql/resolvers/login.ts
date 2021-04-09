44b225670d136ed9a5fa2546c4a30a6fe6174f36
Joao Pedro <joaopedro7423@hotmail.com>
2021-04-09 14:19:33 -0300

    feat: add signUp in graphQl

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,8 +1,12 @@
 import { adaptResolver } from '@/main/adapters'
 import { makeLoginController } from '@/main/factories/controllers/Login/login/login-controller-factory'
+import { makeSignUpController } from '@/main/factories/controllers/Login/signup/signup-controller-factory'
 
 export default {
     Query: {
          login: async (parent: any, args: any) => adaptResolver(makeLoginController(), args)
-    }
+    },
+    Mutation: {
+        signUp: async (parent: any, args: any) => adaptResolver(makeSignUpController(), args)
+   }
 }
