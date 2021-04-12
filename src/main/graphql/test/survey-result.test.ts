b6b0152264ca2c2dacfaf0d13722dedceed0270b
Joao Pedro <joaopedro7423@hotmail.com>
2021-04-12 11:12:42 -0300

    test: ensure surveyResult query returns SaveSurveyResult

--- src/main/graphql/test/survey-result.test.ts
+++ src/main/graphql/test/survey-result.test.ts
@@ -127,4 +127,62 @@ describe('SurveyResult GraphQl', () => {
        expect(res.errors[0].message).toBe('Você não tem acesso a isso.')
       })
   })
+  describe('SaveSurveyResult Mutation', () => {
+    const saveSurveyResultMutation = gql`
+      mutation saveSurveyResult ($surveyId: String!, $answer: String!){
+        saveSurveyResult (surveyId: $surveyId, answer: $answer){
+          question
+          answers {
+            answer
+            count
+            percent
+            isCurrentAccountAnswer
+          }
+          date
+        }
+      }
+    `
+
+    test('should return SurveyResult', async () => {
+      const accessToken = await makeAccessToken()
+      const now = new Date()
+      const surveyRes = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        },{
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      const { mutate } = createTestClient({
+         apolloServer,
+         extendMockRequest: {
+           headers: {
+             'x-access-token': accessToken
+           }
+         }
+      })
+      const res: any = await mutate(saveSurveyResultMutation,{
+        variables: {
+          surveyId: surveyRes.ops[0]._id.toString(),
+          answer: 'Answer 1'
+        }
+      })
+      expect(res.data.saveSurveyResult.question).toBe('Question')
+      expect(res.data.saveSurveyResult.date).toBe(now.toISOString())
+      expect(res.data.saveSurveyResult.answers).toEqual([{
+        answer: 'Answer 1',
+        count: 1,
+        percent: 100,
+        isCurrentAccountAnswer: true
+      },{
+        answer: 'Answer 2',
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }])
+    })
+  })
 })
