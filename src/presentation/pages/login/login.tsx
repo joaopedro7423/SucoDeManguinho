89e0df56c797cf530fc1d7175e85dbcbaad85a7d
Joao Pedro <joaopedro7423@hotmail.com>
2021-04-15 10:22:43 -0300

    refactor: mode header and footer to separete files

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import Styles from './login-styles.scss'
 import Spinner from '@/presentation/pages/components/spinner/spinner'
-import Logo from '../components/logo/logo'
+import LoginHeader from '../components/login-header/login-header'
+import Footer from '@/presentation/pages/components/footer/footer'
 
 const Login: React.FC = () => {
     return (
         <div className={Styles.login}>
-         <header className={Styles.header}>
-            <Logo />
-            <h1>IdioCast - Enquetes para Mongoloides</h1>
-         </header>
+            <LoginHeader />
          <form className={Styles.form}>
             <h2>Login</h2>
             <div className={Styles.inputWrap}>
@@ -27,9 +25,7 @@ const Login: React.FC = () => {
                 <span className={Styles.error}>Erro</span>
             </div>
          </form>
-         <footer className={Styles.footer}>
-
-         </footer>
+          <Footer />
         </div>
     )
 }
