ec6ff7e19114a7bb43937e4b665573fafacd4d6a
Joao Pedro <joaopedro7423@hotmail.com>
2021-04-15 10:41:54 -0300

    refactor: move input to separete files

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Styles from './login-styles.scss'
 import Spinner from '@/presentation/pages/components/spinner/spinner'
-import LoginHeader from '../components/login-header/login-header'
+import LoginHeader from '@/presentation/pages/components/login-header/login-header'
 import Footer from '@/presentation/pages/components/footer/footer'
+import Input from '@/presentation/pages/components/input/input'
 
 const Login: React.FC = () => {
     return (
@@ -10,14 +11,9 @@ const Login: React.FC = () => {
             <LoginHeader />
          <form className={Styles.form}>
             <h2>Login</h2>
-            <div className={Styles.inputWrap}>
-            <input type="email" name="email" id="" placeholder="Digite o seu e-mail"/>
-            <span className={Styles.status}>🖕</span>
-            </div>
-            <div className={Styles.inputWrap}>
-            <input type="password" name="password" id="" placeholder="Digite a sua senha"/>
-            <span className={Styles.status}>🖕</span>
-            </div>
+            <Input type="email" name="email" placeholder="Digite o seu e-mail" />
+            <Input type="password" name="password" placeholder="Digite a sua senha" />
+
             <button type="submit" className={Styles.submit}>Entrar</button>
             <span className={Styles.link}>Criar Conta</span>
             <div className={Styles.errorWrap}>
