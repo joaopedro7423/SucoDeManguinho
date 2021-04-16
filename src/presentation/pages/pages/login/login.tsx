c2dd7bd12edb1cd58052f2ab5fc9ce2186544b4b
Joao Pedro <joaopedro7423@hotmail.com>
2021-04-16 15:59:06 -0300

    feat: ensure Login starts with submit button disabled

--- src/presentation/pages/pages/login/login.tsx
+++ src/presentation/pages/pages/login/login.tsx
@@ -22,7 +22,7 @@ const Login: React.FC = () => {
             <Input type="email" name="email" placeholder="Digite o seu e-mail" />
             <Input type="password" name="password" placeholder="Digite a sua senha" />
 
-            <button type="submit" className={Styles.submit}>Entrar</button>
+            <button data-testid="submit" disabled type="submit" className={Styles.submit}>Entrar</button>
             <span className={Styles.link}>Criar Conta</span>
             <FormStatus />
          </form>
