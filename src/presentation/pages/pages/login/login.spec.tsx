4eea427913e34f06884d9d31ae78d08d2a22c5d5
Joao Pedro <joaopedro7423@hotmail.com>
2021-04-19 15:21:28 -0300

    test: ensure Login calls validation with correct email

--- src/presentation/pages/pages/login/login.spec.tsx
+++ src/presentation/pages/pages/login/login.spec.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
 import Login from './login'
+import { Validation } from '@/presentation/protocols'
 
 type SutTypes = {
         sut: RenderResult
+        validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+        errorMessage: Error
+        input: any
+        validate (input: any): Error {
+                this.input = input
+               return this.errorMessage
+        }
 }
 
 const makeSut = (): SutTypes => {
-        const sut = render(<Login />)
+        const validationSpy = new ValidationSpy()
+        const sut = render(<Login validation={validationSpy} />)
         return {
-                sut
+                sut,
+                validationSpy
         }
 }
 
 describe('Login Component', () => {
-        test('Should not render spinner and error on start', () => {
+        afterEach(cleanup)
+
+        test('Should start with initial state', () => {
            const { sut } = makeSut()
            const errorWrap = sut.getByTestId('error-wrap')
            expect(errorWrap.childElementCount).toBe(0)
@@ -26,5 +41,14 @@ describe('Login Component', () => {
            const passwordStatus = sut.getByTestId('password-status')
            expect(passwordStatus.title).toBe('Campo obrigatório')
            expect(passwordStatus.textContent).toBe('🖕')
+        })
+
+        test('Should call Validation with correct email', () => {
+                const { sut, validationSpy } = makeSut()
+                const emailInput = sut.getByTestId('email')
+                fireEvent.input(emailInput,{ target: { value: 'any_email' } }) // alterando o input de um campo
+                expect(validationSpy.input).toEqual({
+                        email: 'any_email'
+                })
         })
 })
