285dbb252ef5dd04061054dbb4b2c3980cef9265
Joao Pedro <joaopedro7423@hotmail.com>
2021-04-22 10:27:29 -0300

    test: ensure Login navigates to main page on success

--- src/presentation/pages/pages/login/login.spec.tsx
+++ src/presentation/pages/pages/login/login.spec.tsx
@@ -18,7 +18,9 @@ type SutParams = {
         validationError: string
 }
 
-const history = createMemoryHistory()
+const history = createMemoryHistory({
+        initialEntries: ['/login']
+})
 
 const makeSut = (params?: SutParams): SutTypes => { // (params?: ...) Opcional
         const validationStub = new ValidationStubReact()
@@ -158,6 +160,8 @@ describe('Login Component', () => {
                 simulateValidSubmit(sut)
                 await waitFor(() => sut.getByTestId('form'))
                 expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', authenticationSpy.authenticationModel.accessToken)
+                expect(history.length).toBe(1)
+                expect(history.location.pathname).toBe('/')
         })
 
         test('Should go to signup page', () => {
