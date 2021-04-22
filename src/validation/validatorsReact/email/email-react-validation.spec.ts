01f80cb8da56b60b58c5921bbda65ba5de063d57
Joao Pedro <joaopedro7423@hotmail.com>
2021-04-22 16:45:16 -0300

    refactor: move sut creation to a factory method

--- src/validation/validatorsReact/email/email-react-validation.spec.ts
+++ src/validation/validatorsReact/email/email-react-validation.spec.ts
@@ -2,15 +2,17 @@ import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidationR } from './email-react-validation'
 import faker from 'faker'
 
+const makeSut = (): EmailValidationR => new EmailValidationR(faker.random.word())
+
 describe('EmailValidation', () => {
     test('Should return error if email is valid ', () => {
-        const sut = new EmailValidationR(faker.random.word())
+        const sut = makeSut()
         const error = sut.validate(faker.random.word())
         expect(error).toEqual(new InvalidFieldError())
     })
 
     test('Should return false if email is valid ', () => {
-        const sut = new EmailValidationR(faker.random.word())
+        const sut = makeSut()
         const error = sut.validate(faker.internet.email())
         expect(error).toBeFalsy()
     })
