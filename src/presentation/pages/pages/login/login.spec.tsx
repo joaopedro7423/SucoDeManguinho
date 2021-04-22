1dc4477e4e69790a95adb3066fa9248017f82de3
Joao Pedro <joaopedro7423@hotmail.com>
2021-04-22 09:33:27 -0300

    test: ensure Login pressent error and hide spinner on authentication failure

--- src/presentation/pages/pages/login/login.spec.tsx
+++ src/presentation/pages/pages/login/login.spec.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Login from './login'
 import faker from 'faker'
-import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup, waitFor } from '@testing-library/react'
 import { ValidationStubReact } from '@/presentation/test/mock-validation-react'
 import { AuthenticationSpy } from '@/presentation/test'
+import { InvalidCredentialsError } from '@/presentation/errors'
 
 type SutTypes = {
         sut: RenderResult
@@ -127,5 +128,17 @@ describe('Login Component', () => {
                 populateEmailField(sut)
                 fireEvent.submit(sut.getByTestId('form'))
                 expect(authenticationSpy.callsCount).toBe(0)
+        })
+
+        test('Should present error if Authentication fails', async () => {
+                const { sut, authenticationSpy } = makeSut()
+                const error = new InvalidCredentialsError()
+                jest.spyOn(authenticationSpy, 'auth').mockReturnValueOnce(Promise.reject(error))
+                simulateValidSubmit(sut)
+                const errorWrap = sut.getByTestId('error-wrap')
+                await waitFor(() => errorWrap) // isso serve para colocar um observer ai fica aguardando o DOM do elemento mudar quando rodar um test e aparecer para englobar um act usa isso act(...)
+                const mainError = sut.getByTestId('main-error')
+                expect(mainError.textContent).toBe(error.message)
+                expect(errorWrap.childElementCount).toBe(1)
         })
 })
