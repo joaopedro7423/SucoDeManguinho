63020ee81226f1038d411d1c8b21846a3b8b7633
Joao Pedro <joaopedro7423@hotmail.com>
2021-04-23 14:58:37 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validatorsReact/index.ts
@@ -0,0 +1,4 @@
+export * from './email/email-react-validation'
+export * from './min-length/min-length-react-validation'
+export * from './required-field-react/required-field-react-validation'
+export * from './validation-composite/validation-composite-react'
